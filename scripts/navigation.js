13d1e3e317a91b4f51b9dc0dd011629eca5b95fb
jzkeller22 <jzkeller@live.com>
2015-01-24 14:37:05 -0600

    CSS rules, web worker

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -8,4 +8,5 @@ $(document).ready(function() {
 	$("nav").append("<a href='audiovideo.html'>Audio & Video</a> | ");
 	$("nav").append("<a href='funwithcss.html'>Fun with CSS</a> | ");
 	$("nav").append("<a href='geolocation.html'>Geolocation</a> | ");
+	$("nav").append("<a href='WebWorker.html'>Web Worker</a> | ");
 });
